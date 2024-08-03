1988de1af850f411c1334e426ab25a337cbacc0a
wryser <104174626+wryser@users.noreply.github.com>
2024-08-03 23:10:34 +1000

    source code

---
+++ Patches/MapPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OBSWebServer.Patches
+{
+    [HarmonyPatch(typeof(ZoneManagement), "SetZones"), HarmonyWrapSafe]
+    public static class MapPatch
+    {
+        public static GTZone[] ActiveZones;
+
+        public static void Prefix(GTZone[] newActiveZones)
+        {
+            ActiveZones = newActiveZones;
+        }
+    }
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,133 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using System.Net;
+using System.Threading;
+using System.Text;
+using Photon.Pun;
+using System.Linq;
+using OBSWebServer.Patches;
+using BepInEx.Configuration;
+
+namespace OBSWebServer
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+    /// 
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private HttpListener listener;
+        private Thread listenerThread;
+        string roomCode = "Null";
+        string gameMode = "Null";
+        ConfigEntry<bool> showCodePubs;
+        ConfigEntry<bool> showCodePrivates;
+
+        void Awake()
+        {
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+        }
+
+        void OnGameInitialized()
+        {
+            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+            showCodePubs = Config.Bind("Settings", "Show Room Code In Publics", true, "This is whether it should show your room code when you are in a public lobby.");
+            showCodePrivates = Config.Bind("Settings", "Show Room Code In Privates", false, "This is whether it should show your room code when you are in a private lobby.");
+            HarmonyPatches.ApplyHarmonyPatches();
+            listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            listenerThread = new Thread(HandleRequests);
+            listenerThread.Start();
+            Debug.Log("Listening for connections on http://localhost:8080/");
+        }
+
+        void HandleRequests()
+        {
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerResponse response = context.Response;
+                string responseString;
+                if (PhotonNetwork.InRoom)
+                {
+                    string map = FormatMap(MapPatch.ActiveZones.First().ToString().ToUpper());
+                    responseString = $"<html><h1 style=color:white;font-size:300%;font-family:Arial>Gorilla Tag<br>Room Code: {roomCode.ToUpper()}<br>Map: {map}<br>Gamemode: {gameMode}</h1><meta http-equiv=refresh content=4></html>";
+                }
+                else
+                {
+                    responseString = $"<html><h1 style=color:white;font-size:300%;font-family:Arial>Gorilla Tag<br>Not In Room</h1><meta http-equiv=refresh content=4></html>";
+                }
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            listener.Stop();
+            listenerThread.Abort();
+        }
+
+        void Update()
+        {
+            /* Code here runs every frame when the mod is enabled */
+            if (PhotonNetwork.InRoom)
+            {
+                if (PhotonNetwork.CurrentRoom.IsVisible)
+                {
+                    if (showCodePubs.Value)
+                    {
+                        roomCode = PhotonNetwork.CurrentRoom.Name;
+                    }
+                    else
+                    {
+                        roomCode = "CODE HIDDEN";
+                    }
+                }
+                else
+                {
+                    if (showCodePrivates.Value)
+                    {
+                        roomCode = PhotonNetwork.CurrentRoom.Name;
+                    }
+                    else
+                    {
+                        roomCode = "PRIVATE ROOM";
+                    }
+                }
+                gameMode = GorillaGameManager.instance.GameModeName();
+            }
+            else
+            {
+                roomCode = "Not In Room";
+            }
+        }
+        string FormatMap(string map)
+        {
+            string formattedmap;
+            switch (map)
+            {
+                case "CANYON":
+                    formattedmap = "CANYONS";
+                    break;
+                case "CAVE":
+                    formattedmap = "CAVES";
+                    break;
+                case "ROTATING":
+                    formattedmap = "OG CAVES";
+                    break;
+                default:
+                    Debug.Log(map);
+                    formattedmap = map;
+                    break;
+            }
+            return formattedmap;
+        }
+    }
+}
